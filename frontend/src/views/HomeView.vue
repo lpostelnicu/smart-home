12da3a9f8be03403d77621eabbfbd039a1bb7871
Lucian pc <lucian.coding@gmail.com>
2026-01-05 20:11:46 +0100

    initializing Vue Router and adding first page for home

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<template>
+  <h1>Home View</h1>
+  <h2>Here will be the list of houses.</h2>
+
+  <ol>
+    <li>House 1</li>
+    <li>House number 2</li>
+    <li>House Three</li>
+  </ol>
+</template>
+
+<style scoped>
+
+</style>
