6d230a80b238926bc033954d7d38ca0fbababd9e
Lucian pc <lucian.coding@gmail.com>
2026-01-08 00:24:13 +0100

    creating the store for houses list and taking the data in HomeView component

--- frontend/src/store/houses.js
+++ frontend/src/store/houses.js
@@ -1,15 +1,28 @@
 import { defineStore } from "pinia";
+import axiosInstance from "../config/axios.js";
 
 export const useHousesStore = defineStore("housesStore", {
   state: () => ({
-      housesListing: [],
+      isLoading: false,
+      isError: false,
+      housesListing: null,
   }),
   getters: {
-    numberOfHouses: (state) => state.housesListing.length,
+    numberOfHouses: (state) => state.housesListing?.length,
   },
   actions: {
-    async fetchHouses () {
-
+    async fetchHouses() {
+      this.isLoading = true;
+      try {
+        const response = await axiosInstance.get('/houses');
+        console.log("Houses data is ==== ", response.data);
+        this.housesListing = response.data;
+      } catch (error) {
+        console.debug(error);
+        this.isError = true;
+      } finally {
+        this.isLoading = false;
+      }
     },
   },
 })
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,11 +1,37 @@
 <script>
+import { mapState, mapActions } from "pinia"
+import { useHousesStore } from "../store/houses"
 
+export default {
+  name: 'HomeView',
+  props: {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+    ...mapState(useHousesStore, ['housesListing'])
+  },
+  methods: {
+    ...mapActions(useHousesStore, ['fetchHouses'])
+  },
+  async created() {
+    console.log(`The component ${this.$options.name} is created ......`);
+    await this.fetchHouses();
+  },
+  mounted() {
+    console.log(`The component ${this.$options.name} was mounted.`);
+  }
+}
 </script>
 
 <template>
   <h1>Home View</h1>
   <h2>Here will be the list of houses.</h2>
-
+  <p>{{ housesListing }}</p>
   <ol>
     <li>House 1</li>
     <li>House number 2</li>
