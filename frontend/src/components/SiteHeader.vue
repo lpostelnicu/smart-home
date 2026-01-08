cf874d4f6dc66f302e5ba6dc505d4024c4ef5492
Lucian pc <lucian.coding@gmail.com>
2026-01-09 00:48:13 +0100

    working on Homepage (in progress)

--- frontend/src/components/SiteHeader.vue
+++ frontend/src/components/SiteHeader.vue
@@ -19,7 +19,21 @@ export default {
 </script>
 
 <template>
-  <header>
+  <header class="header">
     <a class="logo" href="#">Smart Home</a>
   </header>
 </template>
+
+<style>
+.header {
+  background-color: rgb(229 231 235);
+  text-align: center;
+}
+.logo {
+  font-size: 40px;
+  line-height: 60px;
+  font-weight: 400;
+  display: inline-block;
+  padding: 15px;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script>
 import { mapState, mapActions } from "pinia"
 import { useHousesStore } from "../store/houses"
+// import Button from "primevue/button"
 
 export default {
   name: 'HomeView',
+  components: {
+    
+  },
   props: {
 
   },
@@ -29,16 +33,70 @@ export default {
 </script>
 
 <template>
-  <h1>Home View</h1>
-  <h2>Here will be the list of houses.</h2>
-  <p>{{ housesListing }}</p>
-  <ol>
-    <li>House 1</li>
-    <li>House number 2</li>
-    <li>House Three</li>
-  </ol>
+  <div class="houseListingWrap">
+    <div class="houseListingContainer">
+      <div class="houseListingRow houseListingHead">
+        <div class="houseListingItem houseListingHeadName">Hausname</div>
+        <div class="houseListingItem houseListingHeadLocation">Standort</div>
+        <div class="houseListingItem houseListingHeadRooms">Zimmer</div>
+      </div>
+      <div class="houseListingRow" v-for="h in housesListing" :key="h._id">
+        <div class="houseListingItem houseListingItemName">{{ h.name }}</div>
+        <div class="houseListingItem houseListingItemLocation">{{ h.address.city }}, {{ h.address.country }}</div>
+        <div class="houseListingItem houseListingItemRooms">{{ h.rooms }}</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
+.houseListingContainer {
+  background-color: rgb(248 250 252);
+  border: 0 solid rgb(248 250 252);
+  border-radius: 8px;
+  padding: 10px;
+}
+.houseListingRow {
+  background-color: #ffffff;
+  /* font-size: 14px;
+  line-height: 18px; */
+  border: 1px solid rgb(229 231 235);
+  border-radius: 8px;
+  margin: 0 0 10px 0;
+  vertical-align: text-bottom;
+}
+.houseListingHead {
+  background-color: rgb(248 250 252);
+  border: 0 solid rgb(248 250 252);
+  font-weight: bold;
+  margin: 0;
+}
+.houseListingItem {
+  display: inline-block;
+  padding: 10px 10px;
+}
+.houseListingItemName {
+  width: 55%;
+}
+.houseListingItemLocation {
+  width: 35%;
+  color: #585454;
+}
+.houseListingItemRooms {
+  width: 10%;
+  text-align: right;
+  color: #585454;
+}
+.houseListingHeadName {
+  width: 55%;
+}
+.houseListingHeadLocation {
+  width: 35%;
+  color: #000;
+}
+.houseListingHeadRooms {
+  width: 10%;
+  text-align: right;
+  color: #000;
+}
 </style>
