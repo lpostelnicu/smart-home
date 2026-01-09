081a7b145acc9858fe8556e8ac6e4afe083e8bbc
Lucian pc <lucian.coding@gmail.com>
2026-01-09 15:08:13 +0100

    styling the header and logo

--- frontend/src/components/SiteHeader.vue
+++ frontend/src/components/SiteHeader.vue
@@ -20,20 +20,44 @@ export default {
 
 <template>
   <header class="header">
-    <a class="logo" href="#">Smart Home</a>
+    <a class="logo" href="#">
+      Smart H<span class="oLetter">o</span>me
+    </a>
   </header>
 </template>
 
-<style>
+<style scoped>
 .header {
-  background-color: rgb(229 231 235);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 100%;
+  height: 160px;
+  padding: 10px;
+  background-color: #4F5052;
+  background: center center / cover no-repeat url("../assets/houses-optimised-3.jpg") #4F5052;
+  box-shadow: inset 0 -50px 60px #292828;
   text-align: center;
 }
 .logo {
-  font-size: 40px;
-  line-height: 60px;
-  font-weight: 400;
+  font-family: Blinker, Arial, sans-serif;
+  font-size: 50px;
+  line-height: 50px;
+  font-weight: 700;
   display: inline-block;
   padding: 15px;
+  color: #05a42f;
+  -webkit-text-stroke: 1px #fff;
+}
+@media (min-width: 768px) {
+  .logo {
+    font-size: 90px;
+    line-height: 90px;
+  }
+}
+.logo:hover .oLetter {
+  -webkit-text-fill-color: #fff; /* Will override color (regardless of order) */
+  -webkit-text-stroke: 1px #fff;
+  text-shadow: 3px 0px 0 #fff, 0px 3px 0 #fff, -3px 0px 0 #fff, 0px -3px 0 #fff, 0 0 22px #fff;
 }
 </style>
